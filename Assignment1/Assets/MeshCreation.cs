0a3fc8daa75caaddcc488a25c8cc7ae9393ebdf2
mandyMac <mandytrooster@gmail.com>
2018-02-26 16:36:34 +0100

    added assignment1

---
+++ Assignment1/Assets/MeshCreation.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCreation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        mesh.Clear();
+
+        mesh.vertices = new Vector3[] { 
+			//back 
+			new Vector3(0, 0, 0),	//0
+			new Vector3(0, 1, 0),	//1
+			new Vector3(1, 1, 0),	//2
+			new Vector3(1, 0, 0),	//3
+
+			//top (counterclockwise)
+			new Vector3(1, 1, 1), 	//4
+			new Vector3(0, 1, 1),	//5
+			new Vector3(0, 1, 0),	//6
+			new Vector3(1, 1, 0),	//7
+
+			//front (counterclockwise) 
+			new Vector3(0, 0, 1), 	//8
+			new Vector3(0, 1, 1),	//9
+			new Vector3(1, 1, 1),	//10
+			new Vector3(1, 0, 1),	//11
+
+			//right 
+			new Vector3(0, 0, 1), 	//12
+			new Vector3(0, 1, 1),	//13
+			new Vector3(0, 1, 0),	//14
+			new Vector3(0, 0, 0),	//15
+
+			//left
+			new Vector3(1, 0, 1), 	//16
+			new Vector3(1, 1, 1),	//17
+			new Vector3(1, 1, 0),	//18
+			new Vector3(1, 0, 0),	//19
+
+			//bottom
+			new Vector3(1, 0, 1), 	//20
+			new Vector3(0, 0, 1),	//21
+			new Vector3(0, 0, 0),	//22
+			new Vector3(1, 0, 0)	//23
+
+		};
+       
+		mesh.triangles = new int[] {
+			//back
+			0, 1, 3,
+			3, 1, 2,
+
+			//top
+			5, 4, 7,
+			7, 6, 5,
+
+			//front
+			8, 11, 9,
+			11, 10, 9,
+
+			//right
+			12, 13, 15,
+			14, 15, 13,
+
+			//left
+			17, 16, 19,
+			19, 18, 17,
+
+			//bottom
+			20, 21, 22,
+			22,23, 20
+
+		};
+     
+		mesh.uv = new Vector2[] { 
+			new Vector2(0, 0), 
+			new Vector2(0, 1),
+			new Vector2(1, 1),
+			new Vector2(1,0)
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assignment1/Assets/MeshPulsing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshPulsing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        int i = 0;
+        while (i < vertices.Length)
+        {
+            vertices[i] += normals[i] * Mathf.Sin(Time.time);
+            i++;
+        }
+
+        mesh.vertices = vertices;
+
+	}
+}
