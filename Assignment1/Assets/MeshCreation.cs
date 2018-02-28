fcbb0684b90dc67f9cd75dc9560c37555d03c9e5
mandyMac <mandytrooster@gmail.com>
2018-02-28 19:21:04 +0100

    can change the length of cube

--- Assignment1/Assets/MeshCreation.cs
+++ Assignment1/Assets/MeshCreation.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class MeshCreation : MonoBehaviour {
 
-	private int moveUp;
-	private int moveDown;
+	private int moveHeight;
+	private int moveLength;
 
 	void Start () {
 		gameObject.AddComponent<MeshFilter>();
 		gameObject.AddComponent<MeshRenderer>();	
-		moveUp = 1;
+		moveHeight = 1;
+		moveLength = 1;
 	}
 
 	void Update () {
 
 		if(Input.GetKeyDown(KeyCode.W)){
-			moveUp += 1;
+			moveHeight += 1;
 		}		
 		if(Input.GetKeyDown(KeyCode.S)){
-			moveUp -= 1;
+			moveHeight -= 1;
 		}
 
+		if(Input.GetKeyDown(KeyCode.A)){
+			moveLength += 1;
+		}		
+		if(Input.GetKeyDown(KeyCode.D)){
+			moveLength -= 1;
+		}
 
 		Mesh mesh = GetComponent<MeshFilter>().mesh;
 		mesh.Clear();
@@ -29,39 +36,39 @@ public class MeshCreation : MonoBehaviour {
 		mesh.vertices = new Vector3[] { 
 			//back 
 			new Vector3(0, 0, 0),	//0
-			new Vector3(0, moveUp, 0),	//1
-			new Vector3(1, moveUp, 0),	//2
-			new Vector3(1, 0, 0),	//3
+			new Vector3(0, moveHeight, 0),	//1
+			new Vector3(moveLength, moveHeight, 0),	//2
+			new Vector3(moveLength, 0, 0),	//3
 
 			//top (counterclockwise)
-			new Vector3(1, moveUp, 1), 	//4
-			new Vector3(0, moveUp, 1),	//5
-			new Vector3(0, moveUp, 0),	//6
-			new Vector3(1, moveUp, 0),	//7
+			new Vector3(moveLength, moveHeight, 1), 	//4
+			new Vector3(0, moveHeight, 1),	//5
+			new Vector3(0, moveHeight, 0),	//6
+			new Vector3(moveLength, moveHeight, 0),	//7
 
 			//front (counterclockwise) 
 			new Vector3(0, 0, 1), 	//8
-			new Vector3(0, moveUp, 1),	//9
-			new Vector3(1, moveUp, 1),	//10
-			new Vector3(1, 0, 1),	//11
+			new Vector3(0, moveHeight, 1),	//9
+			new Vector3(moveLength, moveHeight, 1),	//10
+			new Vector3(moveLength, 0, 1),	//11
 
 			//right 
 			new Vector3(0, 0, 1), 	//12
-			new Vector3(0, moveUp, 1),	//13
-			new Vector3(0, moveUp, 0),	//14
+			new Vector3(0, moveHeight, 1),	//13
+			new Vector3(0, moveHeight, 0),	//14
 			new Vector3(0, 0, 0),	//15
 
 			//left
-			new Vector3(1, 0, 1), 	//16
-			new Vector3(1, moveUp, 1),	//17
-			new Vector3(1, moveUp, 0),	//18
-			new Vector3(1, 0, 0),	//19
+			new Vector3(moveLength, 0, 1), 	//16
+			new Vector3(moveLength, moveHeight, 1),	//17
+			new Vector3(moveLength, moveHeight, 0),	//18
+			new Vector3(moveLength, 0, 0),	//19
 
 			//bottom
-			new Vector3(1, 0, 1), 	//20
+			new Vector3(moveLength, 0, 1), 	//20
 			new Vector3(0, 0, 1),	//21
 			new Vector3(0, 0, 0),	//22
-			new Vector3(1, 0, 0)	//23
+			new Vector3(moveLength, 0, 0)	//23
 		};
 			
 		mesh.triangles = new int[] {
